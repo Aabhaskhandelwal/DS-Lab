a3a81d689a77c5d5b5241f13f991249a98e642a6
AabhasK <khandelwalaabhas@gmail.com>
2025-08-17 19:22:58 +0530

    1st assignment commit

---
+++ assignment 1/q4a.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverse(int arr[], int n){
+    for (int i = 0; i < n/2; i++)
+    {
+        int temp=arr[i];
+        arr[i]=arr[n-i-1];
+        arr[n-i-1]=temp;
+    }
+    
+}
+
+int main(){
+    int n;
+    cout<<"enter number of elements"<<endl;
+    cin>>n;
+    int arr[100];
+    cout<<"Enter elements of the array: "<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    reverse(arr,n);
+    cout<<"The reversed array is"<<endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    return 0;
+}
---
+++ assignment 1/q4b.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAX=100;
+
+void matrixMul(int Matrix1[MAX][MAX],int Matrix2[MAX][MAX],int row1,int column1,int row2,int column2){
+    int result[MAX][MAX]={0};
+    for (int i = 0; i < row1; i++) {
+        for (int j = 0; j < column2; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < column1; k++) {
+                result[i][j] += Matrix1[i][k] * Matrix2[k][j];
+            }
+        }
+    }
+
+    cout << "Resultant Matrix:" << endl;
+    for (int i = 0; i < row1; i++) {
+        for (int j = 0; j < column2; j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int row1,column1,row2,column2;
+    cout<<"enter number of rows in 1st matrix"<<endl;
+    cin>>row1;
+    cout<<"enter number of columns in 1st matrix"<<endl;
+    cin>>column1;
+    cout<<"enter number of rows in 2nd matrix"<<endl;
+    cin>>row2;
+    cout<<"enter number of columns in 2nd matrix"<<endl;
+    cin>>column2;
+
+    int matrix1[MAX][MAX];
+    int matrix2[MAX][MAX];
+    if(column1!=row2){
+        cout<<"multiplication not possible";
+        return 0;
+    }
+    for (int i = 0; i < row1; i++)
+    {
+        for (int j = 0; j < column1; j++)
+        {
+            cout<<"enter matrix 1"<<" "<<i<<" "<<j<<endl;
+            cin>>matrix1[i][j];
+        }
+        
+    }
+    for (int i = 0; i < row2; i++)
+    {
+        for (int j = 0; j < column2; j++)
+        {
+            cout<<"enter matrix 2"<<" "<<i<<" "<<j<<endl;
+            cin>>matrix2[i][j];
+        }
+        
+    }
+    
+    matrixMul(matrix1,matrix2,row1,column1,row2,column2);
+
+    return 0;
+}
